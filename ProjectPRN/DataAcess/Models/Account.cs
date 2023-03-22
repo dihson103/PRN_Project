81710e9e874f436461f11c47885c1111ef0a1a2c
Nguyễn Đình Sơn <120614021+dihson103@users.noreply.github.com>
2023-03-22 09:29:28 +0700

    update

---
+++ ProjectPRN/DataAcess/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAcessLayer.Models;
+
+public partial class Account
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public bool Sex { get; set; }
+
+    public string? Address { get; set; }
+
+    public virtual ICollection<Class> Classes { get; } = new List<Class>();
+
+    public virtual ICollection<Grade> Grades { get; } = new List<Grade>();
+
+    public virtual ICollection<Schedule> Schedules { get; } = new List<Schedule>();
+}
---
+++ ProjectPRN/DataAcess/Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAcessLayer.Models;
+
+public partial class Class
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string IdTeacher { get; set; } = null!;
+
+    public int? NumberSlot { get; set; }
+
+    public virtual ICollection<Grade> Grades { get; } = new List<Grade>();
+
+    public virtual Account IdTeacherNavigation { get; set; } = null!;
+
+    public virtual ICollection<Schedule> Schedules { get; } = new List<Schedule>();
+}
